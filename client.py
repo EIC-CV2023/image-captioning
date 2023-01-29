f89981966ddc8ba92802f0fc744370c653d4300d
vikimark <mark.phurit@gmail.com>
2023-01-29 15:51:53 +0700

    test: main & client

--- client.py
+++ client.py
@@ -3,6 +3,12 @@ import cv2
 from custom_socket import CustomSocket
 
 host = socket.gethostname()
-port = 10011
+port = 10012
 c = CustomSocket(host, port)
 c.clientConnect()
+
+data = cv2.imread("test_img/test1.jpg")
+data = cv2.resize(data, (1280, 720))
+
+msg = c.req(data)
+print(msg)
--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import traceback
 
 def main():
     HOST = socket.gethostname()
-    PORT = 10011
+    PORT = 10012
 
     server = CustomSocket(HOST, PORT)
     server.startServer()
@@ -17,5 +17,22 @@ def main():
         conn, addr = server.sock.accept()
         print("Clent connected from", addr)
 
+        while True:
+            try:
+                data = server.recvMsg(conn)
+                img = np.frombuffer(data, dtype=np.uint8).reshape(720, 1280, 3)
+
+                # Show image
+                cv2.imshow("Server Cam", img)
+                if cv2.waitKey(1) == ord('q'):
+                    break
+            except Exception as e:
+                traceback.print_exc()
+                print(e)
+                print("Connection Closed")
+                del res
+                break
+        cv2.destroyAllWindows()
+        
 if __name__ == "__main__":
     main()
